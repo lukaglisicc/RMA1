652b92985f53ffe8291b3d61096d56b4aa1691be
LukaGlisic <lglisic7323rn@raf.rs>
2026-04-16 13:36:59 +0200

    Added scroll to filters screen

--- composeApp/src/commonMain/kotlin/com/example/rma1/filters/FiltersScreen.kt
+++ composeApp/src/commonMain/kotlin/com/example/rma1/filters/FiltersScreen.kt
@@ -12,8 +12,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Button
@@ -146,10 +148,13 @@ private fun FiltersScreenMain (
 
     var selectedGenreId by remember {mutableStateOf(state.filters.genreId)}
 
+    val scrollState = rememberScrollState()
+
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp)
+            .verticalScroll(state = scrollState)
     ) {
 
         Row(
