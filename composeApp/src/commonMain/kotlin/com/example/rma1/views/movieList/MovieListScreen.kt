555c0308f774fd2a1c8da71919cead1a73d79700
LukaGlisic <lglisic7323rn@raf.rs>
2026-05-25 22:33:52 +0200

    Deleted unused imports

--- composeApp/src/commonMain/kotlin/com/example/rma1/views/movieList/MovieListScreen.kt
+++ composeApp/src/commonMain/kotlin/com/example/rma1/views/movieList/MovieListScreen.kt
@@ -13,9 +13,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Button
