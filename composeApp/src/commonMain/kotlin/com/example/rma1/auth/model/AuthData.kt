8a1f34f9596464cc160e5297b58e087acbc16676
LukaGlisic <lglisic7323rn@raf.rs>
2026-05-26 21:12:44 +0200

    Added logout on token expiry

--- composeApp/src/commonMain/kotlin/com/example/rma1/auth/model/AuthData.kt
+++ composeApp/src/commonMain/kotlin/com/example/rma1/auth/model/AuthData.kt
@@ -5,12 +5,10 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class AuthData(
     val accessToken: String? = null,
-    val refreshToken: String? = null,
 ) {
     companion object {
         fun empty(): AuthData = AuthData(
             accessToken = "",
-            refreshToken = "",
         )
     }
 }
--- composeApp/src/commonMain/kotlin/com/example/rma1/di/NetworkModule.kt
+++ composeApp/src/commonMain/kotlin/com/example/rma1/di/NetworkModule.kt
@@ -17,7 +17,6 @@ import io.ktor.client.plugins.api.createClientPlugin
 import io.ktor.client.request.header
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpStatusCode
-import kotlinx.coroutines.runBlocking
 import org.koin.dsl.module
 
 val networkModule = module {
@@ -77,38 +76,9 @@ private fun HttpClientConfig<*>.installAuthPlugin(
                 return@run originalCall
             }
 
-            // Token expired
-            // Trebalo bi da osvezimo access token sa refresh tokenom
-            // ali ovo nece biti potrebno za drugi projekat.
-            // Od prilike bi ovako izgledalo, kada bismo radili:
+            authStoreLazy.value.clearAuthData()
 
-
-
-            val newAuthState = runBlocking {
-                // Zovemo refresh funkciju i vracamo njen rezultat
-//                val authStore = authStoreLazy.value
-//                authStore.refresh()
-
-                // U ovom primeru pretvaramo se da je refresh token istekao
-                AuthState.Unauthenticated
-            }
-
-            @Suppress("IMPOSSIBLE_IS_CHECK_WARNING")
-            when (newAuthState) {
-                is AuthState.Authenticated -> {
-                    // Refresh uspeo - ponavljamo zahtev sa novim tokenom
-                    request.headers.remove(name = HttpHeaders.Authorization)
-                    request.headers.append(
-                        name = HttpHeaders.Authorization,
-                        value = "Bearer ${newAuthState.data.accessToken}",
-                    )
-                    proceed(request)
-                }
-                AuthState.Unauthenticated -> {
-                    // Refresh puko - vracamo originalni 401 response
-                    originalCall
-                }
-            }
+            originalCall
         }
     }
 })
